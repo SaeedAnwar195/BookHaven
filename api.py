b27051f77927cb9bef8e936c2304e6e2f490c5a5
SaeedAnwar195 <saeedanwar123991@gmail.com>
2025-04-28 23:21:54 +0100

    added author api logic

--- api.py
+++ api.py
@@ -4,3 +4,19 @@ from database import db
 from models import Book, Author
 
 api_bp = Blueprint('api', __name__)
+
+@api_bp.route('/authors', methods=['GET'])
+def get_authors():
+    authors = Author.query.all()
+    return jsonify([author.to_dict() for author in authors])
+
+@api_bp.route('/authors', methods=['POST'])
+@login_required
+def create_author():
+    data = request.get_json()
+    if not data or not data.get('name'):
+        return jsonify({'error': 'Name is required'}), 400
+    author = Author(name=data['name'], bio=data.get('bio'))
+    db.session.add(author)
+    db.session.commit()
+    return jsonify(author.to_dict()), 201
