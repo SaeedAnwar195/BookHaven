1765fc7156f1de09bab77f66ab9e877ca67a1ac9
SaeedAnwar195 <saeedanwar123991@gmail.com>
2025-04-28 23:26:33 +0100

    added all the required variables in admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const authorForm     = document.getElementById('authorForm');
+    const bookForm       = document.getElementById('bookForm');
+    const authorList     = document.getElementById('authorList');
+    const bookList       = document.getElementById('bookList');
+    const authorDropdown = document.getElementById('bookAuthorId');
+    const totalBooks     = document.getElementById('totalBooks');
+    const totalAuthors   = document.getElementById('totalAuthors');
+    const authorSection  = document.getElementById('authorSection');
+    const bookSection    = document.getElementById('bookSection');
+
+
 });
