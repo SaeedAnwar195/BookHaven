c6ce38bc6aff96ccf98369a2aff4f22e50f1bdf6
SaeedAnwar195 <saeedanwar123991@gmail.com>
2025-04-30 19:32:24 +0100

    implemented and added data loaders in admin.js file

--- static/js/admin.js
+++ static/js/admin.js
@@ -9,5 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const authorSection  = document.getElementById('authorSection');
     const bookSection    = document.getElementById('bookSection');
 
+    function loadAuthors() {
+        return fetch('/api/authors')
+            .then(res => res.json())
+            .then(authors => {
+                authorDropdown.innerHTML = 
+                    '<option value="">Select Author</option>' +
+                    authors.map(a => `<option value="${a.id}">${a.name}</option>`).join('');
+                authorList.innerHTML = 
+                    '<table border="1"><tr><th>Name</th><th>Bio</th><th>Actions</th></tr>' +
+                    authors.map(a => `
+                        <tr>
+                            <td>${a.name}</td>
+                            <td>${a.bio || ''}</td>
+                            <td>
+                                <button onclick="editAuthor(${a.id})">Edit</button>
+                                <button onclick="deleteAuthor(${a.id})">Delete</button>
+                            </td>
+                        </tr>
+                    `).join('') +
+                    '</table>';
+            })
+            .catch(err => {
+                console.error(err);
+                alert('Error loading authors');
+            });
+    }
+
+    
 
 });
